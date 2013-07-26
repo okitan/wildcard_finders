111a976f2f18fa4dbecbfc0bcdff0fb8583a0362
okitan <okitakunio@gmail.com>
2013-07-26 21:55:58 +0900

    stop using rspec predicator matcher (allowing block to pass)

--- lib/wildcard_finders/matchers.rb
+++ lib/wildcard_finders/matchers.rb
@@ -1,5 +1,5 @@
 require "capybara/node/matchers"
-
+require "tapp"
 module WildcardFinders
   module Matchers
     METHODS = []
@@ -12,6 +12,11 @@ module WildcardFinders
       if method =~ /\Afind_/
         matcher_method    = method.to_s.sub("find", "has") + "?"
         no_matcher_method = method.to_s.sub("find", "has_no") + "?"
+
+        rspec_matcher_method                  = method.to_s.sub("find", "have")
+        rspec_matcher_method_without_block    = method.to_s.sub("find", "have")    + "_without_block"
+        rspec_no_matcher_method               = method.to_s.sub("find", "have_no")
+        rspec_no_matcher_method_without_block = method.to_s.sub("find", "have_no") + "_without_block"
 
         define_method(matcher_method) do |*args, &block|
           wait_method = respond_to?(:synchronize) ? :synchronize : :wait_until
@@ -33,6 +38,27 @@ module WildcardFinders
           end
 
           true
+        end
+
+        RSpec::Matchers.define(rspec_matcher_method_without_block) do |expected|
+          match do |page|
+            page.__send__(matcher_method, expected)
+          end
+        end
+
+        RSpec::Matchers.define(rspec_no_matcher_method_without_block) do |expected|
+          match do |page|
+            page.__send__(no_matcher_method, expected)
+          end
+        end
+
+        # Note: block_given? does not work in define_method
+        RSpec::Matchers.__send__(:define_method, rspec_matcher_method) do |expected = nil, &block|
+          __send__(rspec_matcher_method_without_block, (block ? block : expected))
+        end
+
+        RSpec::Matchers.__send__(:define_method, rspec_no_matcher_method) do |expected = nil, &block|
+          __send__(rspec_no_matcher_method_without_block, (block ? block : expected))
         end
       end
     end
--- spec/wildcard_finders/matchers_spec.rb
+++ spec/wildcard_finders/matchers_spec.rb
@@ -7,11 +7,19 @@ describe WildcardFinders::Matchers do
     it "returns true when there is an anchor" do
       visit "/a"
       page.has_anchor_like?(href: /hoge/).should be_true
+      page.should have_anchor_like(href: /hoge/)
+
+      page.has_anchor_like? {|a| /hoge/ === a[:href] }.should be_true
+      page.should have_anchor_like {|a| /hoge/ === a[:href] }
     end
 
     it "returns true when ther are no anchors" do
       visit "/a"
       page.has_anchor_like?(href: /not_exist/).should be_false
+      page.should_not have_anchor_like(href: /not_exist/)
+
+      page.has_anchor_like? {|a| /not_exist/ === a[:href] }.should be_false
+      page.should_not have_anchor_like {|a| /not_exist/ === a[:href] }
     end
   end
 
@@ -19,11 +27,19 @@ describe WildcardFinders::Matchers do
     it "returns true when there is an anchor" do
       visit "/a"
       page.has_no_anchor_like?(href: /hoge/).should be_false
+      page.should_not have_no_anchor_like(href: /hoge/)
+
+      page.has_no_anchor_like? {|a| /hoge/ === a[:href] }.should be_false
+      page.should_not have_no_anchor_like {|a| /hoge/ === a[:href] }
     end
 
     it "returns true when ther are no anchors" do
       visit "/a"
       page.has_no_anchor_like?(href: /not_exist/).should be_true
+      page.should have_no_anchor_like(href: /not_exist/)
+
+      page.has_no_anchor_like? {|a| /not_exist/ === a[:href] }.should be_true
+      page.should have_no_anchor_like {|a| /not_exist/ === a[:href] }
     end
   end
 end
